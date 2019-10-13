9d4b663ebe75f882294bf9efc418a0e8b768480d
unknown <kevinken@souescoteiro.org.br>
2019-10-13 00:49:10 -0300

    chore: structure of Login and List page. Api reference.

---
+++ mobile/src/components/SpotList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SpotList({tech}){
+    return (
+    <View>
+        <Text>
+            {tech}
+        </Text>
+    </View>
+    )
+}
